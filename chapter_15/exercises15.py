c4bff6351818a5e132bc4ec2c4eb23c8d6c66e6c
ESchreuders <edmschreuders@posteo.nl>
2023-07-31 16:28:14 +0200

    Last time was 15-1, this one is 15-2

--- chapter_15/exercises15.py
+++ chapter_15/exercises15.py
@@ -1,5 +1,6 @@
 import math
 import copy
+import turtle
 
 ################################
 # Chapter 15, in-text exercises
@@ -114,6 +115,81 @@ def rect_circle_corner(rectangle, circle):
         contained = True
     return contained
 
+################################
+# Chapter 4, too lazy for import today
+################################
+def arc(t, r, angle):
+    """Draws an arc with the given radius and angle.
+    t: Turtle
+    r: radius
+    angle: angle subtended by the arc, in degrees
+    """
+    arc_length = 2 * math.pi * r * abs(angle) / 360
+    n = int(arc_length / 4) + 3
+    step_length = arc_length / n
+    step_angle = float(angle) / n
+
+    # making a slight left turn before starting reduces
+    # the error caused by the linear approximation of the arc
+    t.lt(step_angle/2)
+    polyline(t, n, step_length, step_angle)
+    t.rt(step_angle/2)
+    
+def polyline(t, n, length, angle):
+    """Draws n line segments.
+    t: Turtle object
+    n: number of line segments
+    length: length of each segment
+    angle: degrees between segments
+    """
+    for i in range(n):
+        t.fd(length)
+        t.lt(angle)
+
+
+################################
+# Chapter 15-2
+################################
+
+def draw_rect(t, rectangle):
+    '''Takes a rectangle object and uses a turtle to draw it
+       uses: turtle
+       Input: rectangle object and a turtle object
+       Output: rectangle drawn by turtle
+    '''
+    t.penup()
+    t.seth(0)
+    t.fd(rectangle.corner.x)
+    t.lt(90)
+    t.fd(rectangle.corner.y)
+    t.rt(90)
+    t.pendown()
+    for i in range(2):
+        t.fd(rectangle.width)
+        t.lt(90)
+        t.fd(rectangle.height)
+        t.lt(90)
+    t.seth(0)
+
+def draw_circle(t,circle):
+    '''Takes a turtle object and a circle. Draws the circle at the right
+       coordinates. 
+       uses: turtle
+       Input: Turtle object and Circle object
+       Output: Turtle drawing.
+    '''
+    t.seth(0)
+    t.penup()
+    t.fd(circle.center.x)
+    t.lt(90)
+    t.fd(circle.center.y)
+    t.fd(circle.radius)
+    t.lt(90)
+    t.pendown()
+    arc(t, circle.radius, 360)
+    t.rt(90)
+
+
         
 if __name__ == '__main__':    
     ################################
@@ -182,5 +258,31 @@ if __name__ == '__main__':
     print(rect_circle_corner(box3,mycircle))
     
     
+    ################################
+    # Chapter 15 - 2
+    ################################ 
+    bob = turtle.Turtle()
     
+    draw_rect(bob,box)
+    bob.penup()
+    bob.home()
+    bob.pendown()
+    
+    draw_rect(bob,box2)
+    bob.penup()
+    bob.home()
+    bob.pendown()
+    
+    draw_rect(bob,box3)
+    bob.penup()
+    bob.home()
+    bob.pendown()
+    
+    draw_circle(bob, mycircle)
+    
+
+    # wait for the user to close the window
+    turtle.mainloop()
+    
+ 
     
