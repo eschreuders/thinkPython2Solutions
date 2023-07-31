0a2812ab562522376235bb61677c18b713e8aef0
ESchreuders <edmschreuders@posteo.nl>
2023-07-31 14:39:48 +0200

    another in-text exercise

--- chapter_15/exercises15.py
+++ chapter_15/exercises15.py
@@ -1,13 +1,21 @@
 import math
 
 ################################
-# Chapter 15, in-text exercise
+# Chapter 15, in-text exercises
 ################################
 
 class Point:
-    '''Represents a point in 2D space.'''
-    
-    
+    '''Represents a point in 2D space.
+       
+       Attributes: x, y.
+    '''
+
+class Rectangle:
+    '''Represents a rectangle, defined by width, height and coordinates of the 
+       lower left corner
+       
+       Attributes: width, height, corner
+    '''
 
 def distance_between_points(point1, point2):
     '''Takes two point objects and calculates the distance between them.
@@ -19,11 +27,20 @@ def distance_between_points(point1, point2):
     dy = abs(point1.y - point2.y)
     distance = math.sqrt(dx**2 + dy**2)
     return distance
-    
+
+def move_rectangle(rectangle, dx, dy):
+    '''Moves a Rectangle object in the x and y coordinate space.
+       Use negative dx and dy for moving left or down!
+       uses: 
+       Input: rectangle object, <dx> and <dy> both numeric
+       Output: Modified rectangle object (<corner has changed>)
+    '''
+    rectangle.corner.x = rectangle.corner.x + dx
+    rectangle.corner.y = rectangle.corner.y + dy
     
 if __name__ == '__main__':    
     ################################
-    # Chapter 15, in-text exercise
+    # Chapter 15, in-text exercises
     ################################
     Amsterdam = Point()
     Breda = Point()
@@ -32,3 +49,13 @@ if __name__ == '__main__':
     Breda.x = 3
     Breda.y = 2
     print(distance_between_points(Amsterdam, Breda))
+    
+    box = Rectangle()
+    box.width = 100
+    box.height = 50
+    box.corner = Point()
+    box.corner.x = 0
+    box.corner.y = 0
+    
+    move_rectangle(box,10,20)
+    print(box.corner.x, box.corner.y)
